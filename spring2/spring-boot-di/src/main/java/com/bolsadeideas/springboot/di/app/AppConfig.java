f41b8be9b95b6fcd4dc61e9e10081b40b96d2bd8
Javier Garay <javier.garay@2brains.cl>
2020-05-25 13:06:32 -0400

    40. Registrando componentes ItemsFactura

--- spring2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ spring2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -18,5 +23,16 @@ public class AppConfig {
 	@Bean("miServicioComplejo")
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
+	}
+	
+	@Bean("itemFactura")
+	public List<ItemFactura> registrarItems(){
+		 Producto p1 = new Producto("Camara Sony", 100);
+		 Producto p2 = new Producto("Bicicleta Bianchi", 200);
+		 
+		 ItemFactura linea1 = new ItemFactura(p1, 2);
+		 ItemFactura linea2 = new ItemFactura(p2, 4);
+
+		 return Arrays.asList(linea1, linea2);
 	}
 }
