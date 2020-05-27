d89a859cb309064fa1f62403ec0e8cc7a3dfcdb3
Javier Garay <javier.garay@2brains.cl>
2020-05-27 16:16:18 -0400

    52. Validacion del formulario usando anotaciones

--- spring2/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ spring2/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -1,7 +1,13 @@
 package com.bolsadeideas.springboot.form.app.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,8 +22,18 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Usuario usuario, Model model) {		
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {		
 		model.addAttribute("titulo", "Resultado form");
+		
+		if(result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("errores", errores);
+			return "form";
+		}
+		
 		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
